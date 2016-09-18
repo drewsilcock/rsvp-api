4061f7c86f2ec2cb7c31d0f84d3b628c6f7c0e27
drewsberry <drew@drewsilcock.co.uk>
2016-09-18 21:35:11 +0100

    Add RSVP validation for POST, API response creation function.

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, request
 from flask_pymongo import PyMongo
 from bson import json_util
 
@@ -9,30 +9,51 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "rsvp"
 mongo = PyMongo(app)
 
+rsvp_api = {
+    "required_properties": [
+        "name",
+        "can_attend",
+    ],
+}
+
 
 @app.route("/")
-def hello_all():
+def get_all_replies():
     replies = mongo.db.replies.find()
     return make_json_response(replies)
 
 
 @app.route("/name/<name>")
 def get_name_replies(name):
-    replies = mongo.db.replies.find_one_or_404({"name": name});
-    return make_json_response(replies);
+    replies = mongo.db.replies.find_one_or_404({"name": name})
+    return make_json_response(replies)
 
 
 @app.route("/", methods=["POST"])
-def add_person():
-    body = request.get_json()
-    mongo.db.replies.insert_one(body);
-    return make_json_response({"response": "OK", "message": "Successfully added RSVP."})
+def add_reply():
+    new_reply = request.get_json()
+
+    if not rsvp_is_valid(new_reply):
+        return make_api_response(400, "Bad Request", "RSVP is not valid.")
+
+    mongo.db.replies.insert_one(new_reply)
+    return make_api_response(200, "Ok", "Successfully added RSVP.")
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    response = { "response": 404, "message": "RSVP not found." }
-    return make_json_response(response)
+    return make_api_response(404, "Not Found", "RSVP not found.")
+
+
+def rsvp_is_valid(rsvp):
+    """ Test whether an RSVP dictionary has all the required properties. """
+    return all(prop in rsvp for prop in rsvp_api["required_properties"])
+
+
+def make_api_response(code, status, message):
+    return make_json_response(
+        {"status": status,
+         "message": message}), code
 
 
 def make_json_response(cursor):
